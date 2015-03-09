2f8370136c792a89cea45f7aa531675c939443d1
pinz <pinz@mi.fu-berlin.de>
2015-03-09 13:39:12 +0100

    edit burndown.m, sprint3: add comments to octave script and write sprint3 template

--- _doc/burndown-charts/burndown.m
+++ _doc/burndown-charts/burndown.m
@@ -1,22 +1,27 @@
 function burndown (srcFilename,dstFilename)
 
+  % open file with sprint data, count lines and create new cell array with lines-times rows and three coulumns
   file = fopen(srcFilename);
   lines = fskipl(file, Inf);
   frewind(file);
   sprintData = cell(lines, 3);
-
+  
+  % store sprint data in the cell array
   for i = 1:lines
     sprintData{i,1} = fscanf(file, '%d', 1);
     sprintData{i,2} = fscanf(file, '%d', 1);
     sprintData{i,3} = fscanf(file, '%s', 1);
   end
 
+  % initialize arrays to plot the chart
   days = [0:1.0:5];
-  workTotal = 0;
   workDone = [0,0,0,0,0,0];
-
+  
+  %initialize variables
   day = 2;
+  workTotal = 0;
 
+  %sum up the work that was done during the sprint
   for task=1:length(sprintData)
 
     if(day-1 < cell2mat(sprintData(task,1)))
@@ -28,6 +33,7 @@ function burndown (srcFilename,dstFilename)
 
   end
 
+  %the work at the begining of sprint is the totalWork for the sprint
   workDone(1) = workTotal;
 
   for day=2:length(workDone)
@@ -37,6 +43,7 @@ function burndown (srcFilename,dstFilename)
 
   disp(workDone);
 
+  %plot the burndown chart
   figure(1);
   plot(days,workDone);
   grid on;
