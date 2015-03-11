ec0a498356c3b8d17e89898db0e11ee5d0e9414f
Maximilian Stauss <max.stauss@gmail.com>
2015-03-11 13:24:52 +0100

    added js folder structure
    files not moved yet

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ require(__server + 'config/express')(app, express);
 
 // Root route
 app.get('/', function(req, res){
-  res.sendFile('index.html', {root: __client + 'html'});
+  res.sendFile('index.html', {root: __client + 'template'});
 });
 
 // Load routes
