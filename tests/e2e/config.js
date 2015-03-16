45a98bc8133168956bb22a519443422208066036
Aljoscha Pinz <pinz@zedat.fu-berlin.de>
2015-03-16 11:49:51 +0100

    edit juwit-spec.js add reminder to implement suite

--- tests/e2e/config.js
+++ tests/e2e/config.js
@@ -1,6 +1,11 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
+  suites: {
+//    TODO: just one test atm, bundle them to suites later
+//    <suite-name>: '<dir><specname>-spec.js',
+  },
+
   specs: ['juwit-spec.js'],
 
   multiCapabilities: [
