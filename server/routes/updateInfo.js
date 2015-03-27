a417d2f77d1ad7bd7d3a993e486da64be7def754
Maximilian Stauss <max.stauss@gmail.com>
2015-03-27 11:28:53 +0100

    added }

--- server/routes/updateInfo.js
+++ server/routes/updateInfo.js
@@ -1,6 +1,5 @@
 module.exports = function(app) {
-  // Return a list of available node types
-  app.post('/:group/:document/info', function(request, response){
-    response.json({"sender": request.body.sender, "recepient": request.body.recepient, "variballs": true);
+  app.post('/:group/:document/updateLetterInfo', function(request, response){
+    response.json({"sender": request.body.sender, "recepient": request.body.recepient, "variballs": true});
   });
 };
