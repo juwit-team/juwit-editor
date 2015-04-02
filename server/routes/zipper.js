05571fd800b09f86d883f47ad32568266b094876
hoferch91 <hofer.chris91@googlemail.com>
2015-04-02 10:27:15 +0200

    added zipping script to server/routes/zipper.js

---
+++ server/routes/zipper.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var exec = require('child_process').exec;
+    
+    var group = 'company'; 
+    var latexDocumentName = 'document'; // Pfade sind noch statisch. Ersetzten durch die allgemeinen Pfad, siehe updateCSV
+   
+    var changeDir = 'cd ../_texFiles/' + group + '/' + latexDocumentName + '/'; //changing to required folder
+    var directory = 'zipMe'; //name of the directory, that will be zipped
+  
+    var zipDireCmd   = changeDir + ' && ' + 'zip -r Formletter' + Date.now() + ' ./' + directory + '/*.pdf ';
+
+
+exec(zipDireCmd, function(error, stdout, stderr) {
+  if (error !== null) {
+    console.log(error);
+    console.log(stdout);
+    console.log(stderr);
+  }
+});
